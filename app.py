84c1bda330e1a511544fc33e23039a995609464c
pranav.nalawade <pranav.nalawade@apoweri.com>
2025-04-16 23:52:13 +0530

    agent completed t1d

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask_compress import Compress
 from flask import Flask, render_template, request, redirect, session, url_for, flash, send_from_directory, Response
 import os
-
+from codes.main import agent_executor
+from codes.yahoo_finance_helper import ask_yahoo_finance_news
+from codes.ticker_info import ticker_news
 app = Flask(__name__, static_url_path='/static')
 
 Compress(app)
@@ -14,13 +16,27 @@ def index():
 def movers():
     return render_template("top_movers.html")
 
-@app.route('/ai_assistant')
+@app.route('/ai_assistant',methods=['GET', 'POST'])
 def assistant():
+    if request.method == 'POST':
+        query = request.form['query']
+        response = agent_executor.invoke({
+            "input": query
+        })
+        return render_template("ai_assistant.html", response=response, query=query)
+    else:
+        # Handle GET request
+        return render_template("ai_assistant.html")
     return render_template("ai_assistant.html")
 
-@app.route('/news_impact')
+@app.route('/news', methods=['GET', 'POST'])
 def news():
-    return render_template("news_inmpact.html")
+    if request.method == 'POST':
+        query = request.form['query']
+        response = ticker_news(query)
+        return render_template("news.html", response=response, query=query)
+    
+    return render_template("news.html")
 
 
 if __name__ == '__main__':
--- codes/llm.py
+++ codes/llm.py
@@ -1,20 +1,18 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
+from langchain.tools import tool
 
-
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash-001",
+llm = ChatGroq(
+    model="gemma2-9b-it",
     temperature=0,
     max_tokens=None,
     timeout=None,
     max_retries=2,
-    api_key="AIzaSyBmN3fhXAINw0BDubja2qBMB3DT0nUENxw",
-    # other params...
+    api_key="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",
+    
 )
-
 # Define a prompt template
 prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -25,7 +23,9 @@ prompt_template = ChatPromptTemplate.from_messages(
 # Create a chain with the prompt template
 chain = prompt_template | llm
 # Define a function to use the chain
-def ask_gemini(query):
+
+@tool
+def ask_groq(query):
     """
     Ask the Gemini agent a question and get the response.
     """
