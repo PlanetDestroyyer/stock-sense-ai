a804a6d9e47fdcdf9ae871ab306653792b457e58
Pranav <wrieddude@gmail.com>
2025-04-24 21:07:26 +0530

    Update llm.py

--- codes/llm.py
+++ codes/llm.py
@@ -9,7 +9,7 @@ load_dotenv()
 os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="gemma2-9b-it",
     temperature=0,
     max_tokens=None,
     timeout=None,
